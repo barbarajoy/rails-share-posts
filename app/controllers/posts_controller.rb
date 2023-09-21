3021acd40f80b62a8d4494c1dfe50e8d841d2dde
Barbara Joy <barbarajoydneves@gmail.com>
2023-09-21 02:57:22 +0200

    created index and show views and front

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,19 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show]
   def index
     @posts = Post.all
+  end
+
+  def show
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content, :url, :description)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
   end
 end
